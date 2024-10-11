262b4aacca6a1be5dc879b3801892ee5d576985b
xpadev <xpadev@gmail.com>
2024-10-11 15:54:04 +0900

    feat: patch user

--- src/routes/api/v4/users/me.ts
+++ src/routes/api/v4/users/me.ts
@@ -1,6 +1,14 @@
 import {HonoApp} from "@/@types/hono";
+import {z} from "zod";
+import {zValidator} from "@hono/zod-validator";
+import {prisma} from "@/lib/prisma";
 
 export const registerUsersMeRoute = (app: HonoApp) => {
+  registerGet(app);
+  registerPatch(app);
+}
+
+const registerGet = (app: HonoApp) => {
   app.get("/me", async(c) => {
     const user = c.get("user");
     if (!user){
@@ -12,6 +20,35 @@ export const registerUsersMeRoute = (app: HonoApp) => {
     return c.json({
       status: "ok",
       user
+    });
+  });
+}
+
+const PatchSchema = z.object({
+  name: z.string()
+})
+
+const registerPatch = (app: HonoApp) => {
+  app.patch("/me",zValidator("json",PatchSchema), async(c) => {
+    const user = c.get("user");
+    if (!user){
+      return c.json({
+        status: "error",
+        message: "Not logged in"
+      }, 401);
+    }
+    const {name} = c.req.valid("json");
+    await prisma.user.update({
+      where: {
+        id: user.id
+      },
+      data: {
+        name
+      }
+    });
+    return c.json({
+      status: "ok",
+      message: "Name updated"
     });
   });
 }
