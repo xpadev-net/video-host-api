4a7b023ca44c8812ff6aa40d0b9e29106bd732c2
xpadev <xpadev@gmail.com>
2024-10-13 14:41:08 +0900

    chore: register series route

--- src/routes/api/v4/series/index.ts
+++ src/routes/api/v4/series/index.ts
@@ -1,7 +1,9 @@
 import {HonoApp} from "@/@types/hono";
 import {Hono} from "hono";
+import {registerSeriesRoute} from "@/routes/api/v4/series/[series]";
 
 export const registerSeriesRoutes = (app: HonoApp) => {
   const api = new Hono() as HonoApp;
+  registerSeriesRoute(api);
   app.route("/series", api);
 }
