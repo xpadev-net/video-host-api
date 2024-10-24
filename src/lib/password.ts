07926b6a131d3cec1fece65a1054a3a9e02b88f1
xpadev <xpadev@gmail.com>
2024-10-24 16:03:18 +0900

    fix: authorization process

--- src/lib/password.ts
+++ src/lib/password.ts
@@ -4,3 +4,7 @@ import {PASSWORD_HASH_ROUNDS, PASSWORD_SALT} from "@/env";
 export const hashPassword = async (password: string): Promise<string> => {
   return bcrypt.hash(password + PASSWORD_SALT, PASSWORD_HASH_ROUNDS);
 }
+
+export const isPasswordValid = async (password: string, hash: string): Promise<boolean> => {
+  return bcrypt.compare(password + PASSWORD_SALT, hash);
+}
--- src/routes/api/v4/auth.ts
+++ src/routes/api/v4/auth.ts
@@ -2,7 +2,7 @@ import { zValidator } from '@hono/zod-validator'
 import {z} from "zod";
 import {prisma} from "@/lib/prisma";
 import {HonoApp} from "@/@types/hono";
-import {hashPassword} from "@/lib/password";
+import {hashPassword, isPasswordValid} from "@/lib/password";
 import {createSession} from "@/lib/session";
 
 const authSchema = z.object({
@@ -21,20 +21,19 @@ export const registerAuthRoute = (app: HonoApp) => {
         }
       }
     });
-    if(!user) {
+    if(!user || !user.password) {
       return c.json({
         status: "error",
         message: "Invalid username or password",
       }, 401);
     }
-    const passwordHash = await hashPassword(password);
-    if(passwordHash !== user.password) {
+    if(!await isPasswordValid(password, user.password)) {
       return c.json({
         status: "error",
         message: "Invalid username or password",
       }, 401);
     }
-    const token = createSession(user.id);
+    const token = await createSession(user.id);
     return c.json({
       status: "ok",
       token
