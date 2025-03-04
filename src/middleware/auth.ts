1441c7b3529ebcac0ebe1598463a875e6ad49410
xpadev <xpadev@gmail.com>
2025-03-05 08:51:17 +0900

    fix: public endpoint check

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,12 +1,12 @@
-import {prisma} from "@/lib/prisma";
-import {PUBLIC_ENDPOINTS} from "@/env";
-import {createMiddleware} from "hono/factory";
-import {HonoApp} from "@/@types/hono";
-import {unauthorized} from "@/utils/response";
+import { prisma } from "@/lib/prisma";
+import { PUBLIC_ENDPOINTS } from "@/env";
+import { createMiddleware } from "hono/factory";
+import { HonoApp } from "@/@types/hono";
+import { unauthorized } from "@/utils/response";
 
 export const handleAuth = (app: HonoApp) => {
   app.use("/*", authMiddleware);
-}
+};
 
 const authMiddleware = createMiddleware<{
   Variables: {
@@ -14,13 +14,13 @@ const authMiddleware = createMiddleware<{
       id: string;
       username: string;
       name: string;
-    }
-  }
-}>(async(c, next) => {
-  const url = (new URL(c.req.url)).pathname;
+    };
+  };
+}>(async (c, next) => {
+  const url = new URL(c.req.url).pathname;
   const token = c.req.header().authorization?.replace("Bearer ", "");
-  if (!token){
-    if (isPublicEndpoint(url)){
+  if (!token) {
+    if (isPublicEndpoint(url)) {
       await next();
       return;
     }
@@ -28,13 +28,17 @@ const authMiddleware = createMiddleware<{
   }
   const session = await prisma.session.findFirst({
     where: {
-      token
+      token,
     },
     include: {
-      user: true
+      user: true,
+    },
+  });
+  if (!session) {
+    if (isPublicEndpoint(url)) {
+      await next();
+      return;
     }
-  })
-  if (!session){
     return unauthorized(c, "Unauthorized");
   }
   c.set("user", session.user);
@@ -42,10 +46,10 @@ const authMiddleware = createMiddleware<{
 });
 
 const isPublicEndpoint = (url: string) => {
-  for (const publicPath of PUBLIC_ENDPOINTS){
-    if (url.startsWith(publicPath)){
+  for (const publicPath of PUBLIC_ENDPOINTS) {
+    if (url.startsWith(publicPath)) {
       return true;
     }
   }
   return false;
-}
+};
