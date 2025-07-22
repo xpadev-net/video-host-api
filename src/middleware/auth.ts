6697b92f581bb11a1888b34c0c55989f12d6cd64
xpadev <xpadev@gmail.com>
2025-07-22 09:24:00 +0900

    fix: token expiration

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,51 +5,54 @@ import { HonoApp } from "@/@types/hono";
 import { unauthorized } from "@/utils/response";
 
 export const handleAuth = (app: HonoApp) => {
-  app.use("/*", authMiddleware);
+	app.use("/*", authMiddleware);
 };
 
 const authMiddleware = createMiddleware<{
-  Variables: {
-    user: {
-      id: string;
-      username: string;
-      name: string;
-    };
-  };
+	Variables: {
+		user: {
+			id: string;
+			username: string;
+			name: string;
+		};
+	};
 }>(async (c, next) => {
-  const url = new URL(c.req.url).pathname;
-  const token = c.req.header().authorization?.replace("Bearer ", "");
-  if (!token) {
-    if (isPublicEndpoint(url)) {
-      await next();
-      return;
-    }
-    return unauthorized(c, "Unauthorized");
-  }
-  const session = await prisma.session.findFirst({
-    where: {
-      token,
-    },
-    include: {
-      user: true,
-    },
-  });
-  if (!session) {
-    if (isPublicEndpoint(url)) {
-      await next();
-      return;
-    }
-    return unauthorized(c, "Unauthorized");
-  }
-  c.set("user", session.user);
-  await next();
+	const url = new URL(c.req.url).pathname;
+	const token = c.req.header().authorization?.replace("Bearer ", "");
+	if (!token) {
+		if (isPublicEndpoint(url)) {
+			await next();
+			return;
+		}
+		return unauthorized(c, "Unauthorized");
+	}
+	const session = await prisma.session.findFirst({
+		where: {
+			token,
+			expiredAt: {
+				gt: new Date(),
+			},
+		},
+		include: {
+			user: true,
+		},
+	});
+	if (!session) {
+		if (isPublicEndpoint(url)) {
+			await next();
+			return;
+		}
+		return unauthorized(c, "Unauthorized");
+	}
+	c.set("user", session.user);
+	await next();
 });
 
 const isPublicEndpoint = (url: string) => {
-  for (const publicPath of PUBLIC_ENDPOINTS) {
-    if (url.startsWith(publicPath)) {
-      return true;
-    }
-  }
-  return false;
+	for (const publicPath of PUBLIC_ENDPOINTS) {
+		if (url.startsWith(publicPath)) {
+			return true;
+		}
+	}
+	return false;
 };
